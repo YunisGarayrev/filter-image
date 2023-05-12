0d9bd2b18a22ee983a58f502be91d14b6845b708
YunisGarayrev <127491675+YunisGarayrev@users.noreply.github.com>
2023-05-12 10:35:37 -0700

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,7 @@
 
 var sort=document.getElementById("sort")
 var filter= document.getElementById("filter-part");
+    filter.style.display="none"
 sort.addEventListener("mouseenter", e=>{
     filter.style.display="flex"
 });
@@ -8,10 +9,77 @@ sort.addEventListener("mouseleave", e=>{
     filter.style.display="none"
 });
 
-var all=document.getElementById("all")
-var cont=document.getElementsByClassName("container-1")
-all.addEventListener("click",el=>{
-    var all=document.getElementById("all")
-var cont=document.getElementsByClassName("container-1")
-cont.setAttribute('d-none', el.getAttribute("d-flex"));
-    })
+function myFunction2() {
+  var image = document.querySelectorAll(".foto");
+  image.forEach(function(e) {
+    if (e.id !== "cactuses") {
+      e.style.display = "none";
+    } else {
+      e.style.display = "block";
+    }
+  });
+}
+//   ilk ve ikinci kodda cuzi fer1 var
+function myFunction3() {
+  var image = document.querySelectorAll(".foto");
+  image.forEach(function(e) {
+      if (e.id == "exotic") {
+        e.style.display = "block";
+  } else {
+      e.style.display = "none";
+    }
+  });
+}
+
+function myFunction4() {
+  var image = document.querySelectorAll(".foto");
+  image.forEach(function(e) {
+      if (e.id == "greens") {
+        e.style.display = "block";
+  } else {
+      e.style.display = "none";
+    }
+  });
+}
+
+function myFunction5() {
+  var image = document.querySelectorAll(".foto");
+  image.forEach(function(e) {
+      if (e.id == "popular") {
+        e.style.display = "block";
+  } else {
+      e.style.display = "none";
+    }
+  });
+}
+
+function myFunction6() {
+  var image = document.querySelectorAll(".foto");
+  image.forEach(function(e) {
+      if (e.id == "various") {
+        e.style.display = "block";
+  } else {
+      e.style.display = "none";
+    }
+  });
+}
+function myFunction7() {
+  var image = document.querySelectorAll(".foto");
+  image.forEach(function(e) {
+      if (e.id == "winter") {
+        e.style.display = "block";
+  } else {
+      e.style.display = "none";
+    }
+  });
+}
+function myFunction1() {
+  var image = document.querySelectorAll(".foto");
+  image.forEach(function(e) {
+      if (e.id == "every") {
+        e.style.display = "none";
+  } else {
+      e.style.display = "block";
+    }
+  });
+}
